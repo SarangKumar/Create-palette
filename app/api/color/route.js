0dd62db9c3b969de51eefb3cd581a0808ba05fb2
SarangKumar <sarangkumar1578@gmail.com>
2023-06-29 00:25:53 +0530

    minor route change

--- app/api/color/route.js
+++ app/api/color/route.js
@@ -4,7 +4,7 @@ import { connectToDB } from "@utils/database";
 export const GET = async (req) => {
 	try {
 		await connectToDB();
-		const palettes = await Color.find({});
+		const palettes = await Color.find({}).populate('creator');
 		// console.log(palettes);
 		return new Response(JSON.stringify(palettes), { status: 200 });
 	} catch (error) {
